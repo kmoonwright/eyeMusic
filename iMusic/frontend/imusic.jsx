c6ea3d12b2fe8abe1b6eeb3e735c25d56e80d475
kmoonwright <kmoonwright@outlook.com>
2019-09-02 16:55:55 -0700

    Add login functionality to html

--- iMusic/frontend/imusic.jsx
+++ iMusic/frontend/imusic.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './components/root';
-import configureStore from './store/store';
+import createStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-
-    const store = configureStore();
+    let preloadedState = undefined;
+    if (window.currentUser) {
+        preloadedState = {
+            session: {
+                currentUser: window.currentUser
+            }
+        }
+    }
+    const store = createStore(preloadedState);
 
     ReactDOM.render(<Root store={store} />, root)
 });
