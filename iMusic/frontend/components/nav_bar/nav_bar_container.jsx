cbdc7a228e669218a3ccd426671cbf5ea5f99110
kmoonwright <kmoonwright@outlook.com>
2019-09-02 16:47:41 -0700

    Add NavBar components

---
+++ iMusic/frontend/components/nav_bar/nav_bar_container.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import NavBar from './nav_bar';
+import { logout } from '../../actions/session'
+
+const msp = state => ({
+    currentUser: state.session.currentUser
+});
+
+const mdp = dispatch => ({
+    logout: () => dispatch(logout())
+})
+
+export default connect(msp, mdp)(NavBar);
