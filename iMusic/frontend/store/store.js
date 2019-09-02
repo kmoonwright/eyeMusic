4a81a65c76b63142bd519a0f426426640daa9ef6
kmoonwright <kmoonwright@outlook.com>
2019-09-02 14:19:49 -0700

    Add thunk functionality through module

--- iMusic/frontend/store/store.js
+++ iMusic/frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 
-import RootReducer from '../reducers/root';
-import thunk from '../thunk/thunk';
+import RootReducer from '../reducers/root_reducer.js';
+import thunk from 'redux-thunk';
 
 export default (preloadedState = {}) => {
     return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
