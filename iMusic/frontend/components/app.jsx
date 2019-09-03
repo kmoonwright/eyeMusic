a24d18f6697aaaf4198a65f5de4fcbc21b3b90f4
kmoonwright <kmoonwright@outlook.com>
2019-09-03 11:18:23 -0700

    Add landing page and route

--- iMusic/frontend/components/app.jsx
+++ iMusic/frontend/components/app.jsx
@@ -6,18 +6,14 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import SignUpContainer from './session/signup_container'
 import LoginContainer from './session/login_container'
+import LandingPage from './landing_page/landing_page';
 
 const App = () => (
     <div>
         <Route path="/" component={NavBarContainer} />
+        <Route exact path="/" component={LandingPage} />
         <AuthRoute path="/login" component={LoginContainer} />
         <AuthRoute path="/signup" component={SignUpContainer} />
-
-        <h1>VVELC0ME</h1>
-        <ul>
-            <li>DIS IS</li>
-            <li>APP APP</li>
-        </ul>
     </div>
 );
 
---
+++ iMusic/frontend/components/landing_page/landing_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class LandingPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>VVELC0ME</h1>
+                <ul>
+                    <li>DIS IS</li>
+                    <li>APP APP</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
