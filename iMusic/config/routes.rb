3b8158437e2530505561780a894924c475adbb8a
kmoonwright <kmoonwright@outlook.com>
2019-09-03 18:21:32 -0700

    Tidy routes

--- iMusic/config/routes.rb
+++ iMusic/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create]
-    resource :session, only: [:new, :create, :destroy]
+    resources :users, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy]
   end
 
   root to: "root#root"
