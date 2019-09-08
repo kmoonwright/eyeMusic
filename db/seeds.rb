3df87e3c1aa0d84829f17423f80075582e523046
kmoonwright <kmoonwright@outlook.com>
2019-09-08 13:24:24 -0700

    Updating seed data format

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# DESTROY EVERYTHING
 
+User.destroy_all
+Song.destroy_all
+Album.destroy_all
+Artist.destroy_all
+Playlist.destroy_all
+PlaylistSong.destroy_all
 
-demoUser = User.create(username: 'demoUser', password: '123456')
-test = User.create(username: 'test',  password: '123456')
-test = User.create(username: 'test5',  password: '123456')
+# USERS
 
-testArtist1 = Artist.create(name:"Prince")
-testArtist2 = Artist.create(name:"Daft Punk")
-testArtist3 = Artist.create(name:"Jimi Hendrix Experience")
-testAlbum1 = Album.create(title:"1999", artist_id: testArtist1.id)
-testAlbum2 = Album.create(title:"Discovery", artist_id: testArtist2.id)
-testAlbum3 = Album.create(title:"Are You Experienced?", artist_id: testArtist3.id)
-testSong1 = Song.create(title:"D.M.S.R", album_id: testAlbum1.id)
-testSong2 = Song.create(title:"One More Time", album_id: testAlbum2.id)
-testSong3 = Song.create(title:"Manic Depression", album_id: testAlbum3.id)
+# demoUser = User.create(username: 'demoUser', password: '123456')
+# test = User.create(username: 'test',  password: '123456')
+# test = User.create(username: 'test5',  password: '123456')
+
+# Daft Punk
+daftpunk = Artist.create(name:'Daft Punk')
+
+discovery = Album.create(name:'Discovery', artist_id: daftpunk.id)
+
+onemoretime = Song.create(title:'One More Time', album_id: testAlbum2.id)
+
+# Jimi Hendrix
+jimihendrixexperience = Artist.create(name:'Jimi Hendrix Experience')
+
+areyouexperienced = Album.create(name:'Are You Experienced?', artist_id: jimihendrixexperience.id)
+
+manicdepression = Song.create(title:'Manic Depression', album_id: areyouexperienced.id)
+
+# Prince
+
+prince = Artist.create(name:"Prince")
+
+nineteenninetynine = Album.create(title:"1999", artist_id: prince.id)
+
+dmsr = Song.create(title:"D.M.S.R", album_id: nineteenninetynine.id)
+
