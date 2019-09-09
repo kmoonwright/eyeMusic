d6569d5f918946735368528844954506219f640f
kmoonwright <kmoonwright@outlook.com>
2019-09-08 17:17:31 -0700

    Add routes for newly created music_actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resources :users
-    
     resource :session, only: [:create, :destroy]
+
+    resources :users
+    resources :songs, only: [:index, :show]
+    resources :albums, only: [:index, :show]
+    resources :artists, only: [:index, :show]
+    resources :playlist, only: [:index, :show, :create, :destroy]
   end
 
   root to: "root#root"
