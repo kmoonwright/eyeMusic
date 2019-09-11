416b561d20f8b18f4fc486372f9d00c133bbb9bf
kmoonwright <kmoonwright@outlook.com>
2019-09-10 20:52:28 -0700

    Add playlist controller

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -8,10 +8,28 @@ class Api:PlaylistsController < ApplicationController
         @playlist = Playlist.find_by(id: params[:id])
     end
 
-    # def create
-    # end
+    def create
+        @playlist = Playlist.new(playlists_params)
+        @playlist.user_id = current_user.id
 
-    # def destroy
-    # end
+        if @playlist.save
+            render "api/playlists/new"
+        else
+            render json: @playlist.errors.full_messages, status: 422
+        end
+    end
 
+    def destroy
+        @playlist = Playlist.find(params[:id])
+        if @playlist
+            @playlist.destroy
+        else
+            render json: ['Playlist cannot be found']
+        end
+    end
+
+    private
+    def playlists_params
+        params.require(:playlist).permit(:title, :user_id)
+    end
 end
